ea56c5de7859e0843ee27bee21033ba8d402470a
emrahkiziltan94 <138603299+emrahkiziltan94@users.noreply.github.com>
2024-06-26 11:06:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# component-demo
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/emrahkiziltan94/component-demo)
---
+++ script.js
@@ -0,0 +1,33 @@
+let productList = [
+  { name: 'pc', price: 1200, backgroundColor: 'red' },
+  { name: 'phone', price: 600, backgroundColor: 'blue' },
+  { name: 'tablet', price: 1400, backgroundColor: 'yellow' },
+  { name: 'klavye', price: 300, backgroundColor: 'pink' },
+  { name: 'pc', price: 1200, backgroundColor: 'aqua' },
+  { name: 'pc', price: 1200, backgroundColor: 'red' },
+];
+
+/*
+<div style="background-color:{backgroundColor}">
+  <h3>{name}</h3>
+  <p>{price}</p>
+</div>
+*/
+
+const container = document.getElementById('container');
+productList.forEach((product) => {
+  const divItem = getDivItem(product);
+  container.appendChild(divItem);
+});
+
+function getDivItem(product) {
+  const divItem = document.createElement('div');
+  divItem.style.backgroundColor = product.backgroundColor;
+  const h3Title = document.createElement('h3');
+  h3Title.textContent = product.name;
+  divItem.appendChild(h3Title);
+  const pPrice = document.createElement('p');
+  pPrice.textContent = product.price;
+  divItem.appendChild(pPrice);
+  return divItem;
+}
